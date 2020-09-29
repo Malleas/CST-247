6c1430bec32e079c274e75d37c049532149b5244
Matt Sievers <msievers2@my.gcu.edu>
2020-09-28 21:38:30 -0500

    Week 3 Activity 2 part 1

---
+++ Activity2Part1/Activity2Part1/Controllers/ButtonController.cs
@@ -0,0 +1,55 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    
+    public class ButtonController : Controller
+    {
+        static public List<ButtonModel> buttons = new List<ButtonModel>();
+        // GET: Button
+        public ActionResult Index()
+        {
+            ButtonModel b1 = new ButtonModel(true);
+            ButtonModel b2 = new ButtonModel(false);
+
+            buttons.Add(b1);
+            buttons.Add(b2);
+            return View("Button", buttons);
+        }
+
+
+        //POST Button/OnButtonClick
+
+        [HttpPost]
+
+        public ActionResult OnButtonClick(string mine)
+        {
+            if(mine.Equals("1")) {
+                if (!buttons[0].State)
+                {
+                    buttons[0].State = true;
+                }
+                else
+                {
+                    buttons[0].State = false;
+                }
+            }else if (mine.Equals("2"))
+            {
+                if (!buttons[1].State)
+                {
+                    buttons[1].State = true;
+                }
+                else
+                {
+                    buttons[1].State = false;
+                }
+            }
+            return View("Button", buttons);
+        }
+    }
+}
---
+++ Activity2Part1/Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> userList = new List<UserModel>();
+            UserModel user = new UserModel();
+
+            user.Name = "Bob";
+            user.EmailAddress = "me@home.com";
+            user.PhoneNumber = "1234567890";
+            userList.Add(user);
+
+           
+            
+            return View("Test", userList);
+        }
+    }
+}
