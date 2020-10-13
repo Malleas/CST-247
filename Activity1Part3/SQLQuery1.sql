cd7aa598e7fe74d5fd96847505a1723dea4f7c4d
Matt Sievers <msievers2@my.gcu.edu>
2020-10-12 21:50:17 -0500

    Week 5 Activity 3 Part 1 Working with Ajax initial commit

--- Activity1Part3/SQLQuery1.sql
+++ Activity1Part3/SQLQuery1.sql
@@ -1,5 +1,1 @@
-﻿select * from dbo.Users;
-
-insert into dbo.Users(USERNAME, PASSWORD) values('test', 'test');
-
-select * from dbo.Users where USERNAME = 'test';
+﻿select * from dbo.Users
---
+++ Activity3Part1/Activity3Part1/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using Activity3Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity3Part1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Customer customer;
+        List<Customer> customerList;
+       
+        public CustomerController()
+        {
+            customerList = new List<Customer>();
+            customerList.Add(new Customer(0, "Bob", 27));
+            customerList.Add(new Customer(1, "Matt", 38));
+            customerList.Add(new Customer(2, "Sue", 62));
+        }
+       
+        
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+            Tuple<List<Customer>, Customer> tuple;
+            tuple = new Tuple<List<Customer>, Customer>(customerList, customerList[0]);
+            return View("Customer", tuple);
+        }
+
+        [HttpPost]
+        public ActionResult OnSelectCustomer(string CustomerNumber)
+        {
+           
+
+            return PartialView("_CustomerDetails", customerList[Int32.Parse(CustomerNumber)]);
+        }
+
+        [HttpPost]
+
+        public ActionResult GetMoreInfo(string CustomerNumber)
+        {
+            return PartialView("_CustomerDetails", "Hello World");
+           
+        }
+    }
+}
