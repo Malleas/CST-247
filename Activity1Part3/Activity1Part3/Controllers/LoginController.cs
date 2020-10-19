f115b9a5dfa474febe96699c4b416fb1b596fe7f
Matt Sievers <msievers2@my.gcu.edu>
2020-10-19 17:34:34 -0500

    Week 6 Assignment 4 Part 1 Adding logging

--- Activity1Part3/Activity1Part3/Controllers/LoginController.cs
+++ Activity1Part3/Activity1Part3/Controllers/LoginController.cs
@@ -5,11 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NLog;
+using System.Web.Script.Serialization;
+using Activity1Part3.Services.Utility;
 
 namespace Activity1Part3.Controllers
 {
     public class LoginController : Controller
     {
+        
         // GET: Login
         [HttpGet]
         public ActionResult Index()
@@ -20,19 +24,33 @@ namespace Activity1Part3.Controllers
         [HttpPost]
         public ActionResult Login(UserModel user)
         {
-            //Validate the Form POST
-            if (!ModelState.IsValid)
-                return View("Login");
-            SecurityService service = new SecurityService();
-            bool result = service.Authenticate(user);
+            MyLogger.GetInstance().Info("Entering LoginController.Login Using the new Singleton MyLogger Class");
 
-            if (result)
-            {
-                return View("LoginPassed");
+            try {
+
+                if (!ModelState.IsValid)
+                {
+                    return View("Login");
+                }
+                SecurityService service = new SecurityService();
+                MyLogger.GetInstance().Info("Parameters are: " + new JavaScriptSerializer().Serialize(user));
+                bool result = service.Authenticate(user);
+
+
+                if (result)
+                {
+                    MyLogger.GetInstance().Info("Exit login controller.  Successful login");
+                    return View("LoginPassed");
+                }
+                else
+                {
+                    MyLogger.GetInstance().Info("Exit login controller.  Login failed.");
+                    return View("LoginFailed");
+                }
             }
-            else
-            {
-                return View("LoginFailed");
+            catch (Exception e) {
+                MyLogger.GetInstance().Error("Exception!" + e.Message);
+                return Content("Exception in login" + e.Message);
             }
         }
     }
